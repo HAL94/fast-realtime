6f58499eef512817b85c2041f8d8a869f774a14c
HAL94 <hqdarwish@gmail.com>
2025-04-13 00:38:13 +0300

    basic init of redis pubsub

---
+++ app/redis/pubsub.py
@@ -0,0 +1,28 @@
+import asyncio
+from fastapi import Depends
+import redis.asyncio as AsyncRedis
+
+from app.redis.client import get_redis_client
+
+
+def get_pubsub(client: AsyncRedis.Redis = Depends(get_redis_client)):
+    return RedisPubsub(client=client)
+
+
+class RedisPubsub:
+    def __init__(self, client: AsyncRedis.Redis):
+        self.pubsub = client.pubsub()
+
+    async def subscribe(self, channel: str, *args, **kwargs):
+        await self.pubsub.subscribe(channel, args=args, kwargs=kwargs)
+
+    def listen(self, listener_func, *args, **kwargs):
+        async def run_task():
+            async for message in self.pubsub.listen():
+                if message["type"] == "message":
+                    data = message["data"].decode("utf-8")
+                    await listener_func(data, *args, **kwargs)
+
+        listener_task = asyncio.create_task(run_task())
+
+        return listener_task
--- app/websocket/v1/scores/ws_route.py
+++ app/websocket/v1/scores/ws_route.py
@@ -2,15 +2,18 @@
 # from app.redis.channels import SCORES_CHANNEL
 # from app.websocket.v1.scores.utils import publish
 
+
+# import asyncio
 from fastapi import APIRouter, Depends, WebSocket
 import redis.asyncio as AsyncRedis
 
-from app.core.auth.schema import UserRead
-from app.core.auth.websocket import validate_ws_jwt
+# from app.core.auth.schema import UserRead
+# from app.core.auth.websocket import validate_ws_jwt
 from app.redis.client import get_redis_client
 
 import logging
 
+from app.redis.pubsub import RedisPubsub, get_pubsub
 from app.websocket.v1.scores.service import ScoreService, get_score_service
 
 logger = logging.getLogger("uvicorn")
@@ -19,7 +22,7 @@ router = APIRouter()
 
 
 @router.websocket("/")
-async def websocket_endpoint(
+async def ws_welcome(
     websocket: WebSocket, redis: AsyncRedis.Redis = Depends(get_redis_client)
 ):
     await websocket.accept()
@@ -35,23 +38,50 @@ async def websocket_endpoint(
         await websocket.send_text(f"Message text was: {data}")
 
 
+@router.websocket("/add-score")
+async def ws_submit_score(
+    websocket: WebSocket, redis: AsyncRedis.Redis = Depends(get_redis_client)
+):
+    await websocket.accept()
+
+    while True:
+        data = await websocket.receive_text()
+
+        await redis.publish("score_submission", message=data)
+
+        await websocket.send_text("done")
+
+
 @router.websocket("/scores")
-async def websocket_user1_score(
+async def ws_get_scores(
     websocket: WebSocket,
     score_service: ScoreService = Depends(get_score_service),
-    user_data: UserRead | None = Depends(validate_ws_jwt),
+    # user_data: UserRead | None = Depends(validate_ws_jwt),
+    pubsub: RedisPubsub = Depends(get_pubsub),
 ):
-    if not user_data:
-        # gracefylly exit endpoint
-        return
+    await websocket.accept()
+    # if not user_data:
+    # gracefylly exit endpoint
+    # return
+
+    await pubsub.subscribe("score_submission")
+
+    async def score_submission_listener(data):
+        print(f"got the passed score by user: {data}")
+        result = await score_service.get_leaderboard_data("all")
+        print(f"got result: {result}")
+        await websocket.send_json({"result": result})
 
     try:
-        while True:            
+        pubsub.listen(score_submission_listener)
+
+        while True:
             payload: dict = await websocket.receive_json()
             print(f"payload for filtering: {payload.get('game')}")
-            result = await score_service.get_leaderboard_data(payload.get('game'))
-            print(f"got result: {result}")
+            result = await score_service.get_leaderboard_data(payload.get("game"))
+            # print(f"got result: {result}")
             await websocket.send_json({"result": result})
+
     except AsyncRedis.ConnectionError as e:
         logger.error(f"Redis connection error: {e}")
     except Exception as e:
