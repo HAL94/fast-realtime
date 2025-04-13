e948197861638da06ef39b92ea0cd6f94435184f
HAL94 <hqdarwish@gmail.com>
2025-04-13 20:40:47 +0300

    Update: a model that sets the camelCase

--- app/core/common/app_response.py
+++ app/core/common/app_response.py
@@ -4,25 +4,16 @@ from pydantic.alias_generators import to_camel
 
 T = TypeVar('T')
 
-
-class AppResponse(BaseModel, Generic[T]):
-    success: bool = Field(description="Is operation success", default=True)
-    status_code: int = Field(description="status code", default=200)
-    message: str = Field(description="Message back to client", default="done")
-    
-    data: Optional[T] = None
-    
+class ModelCamelized(BaseModel):
     model_config = ConfigDict(
         alias_generator=AliasGenerator(to_camel),
         populate_by_name=True,
     )
 
-    def __dict__(self):
-        return {
-            "success": self.success,
-            "statusCode": self.status_code,
-            "message": self.message,
-            "data": self.data
-        }
+class AppResponse(Generic[T], ModelCamelized):
+    success: bool = Field(description="Is operation success", default=True)
+    status_code: int = Field(description="status code", default=200)
+    message: str = Field(description="Message back to client", default="done")
+    data: Optional[T] = None
     
         
