e7b0c35303a4d0a703daf168f398f1bc6278d30b
Habib Aldarwish <habib.aldarwish@ramzaula.com>
2025-04-23 15:10:20 +0300

    Update: top user score should update if user updates any of his personal scores

--- app/websocket/v1/scores/ws_route.py
+++ app/websocket/v1/scores/ws_route.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
 import redis.asyncio as AsyncRedis
 
@@ -9,6 +10,7 @@ from app.redis.client import get_redis_client
 
 import logging
 
+from app.redis.events import SCORE_SUBMISSION, USER_SCORE
 from app.redis.pubsub import RedisPubsub, get_pubsub
 from app.websocket.v1.scores.schema import SubmitScore
 from app.websocket.v1.scores.service import ScoreService, get_score_service
@@ -38,21 +40,38 @@ async def ws_user_score(
     websocket: WebSocket,
     scores_service: ScoreService = Depends(get_score_service),
     user_data: UserRead = Depends(validate_ws_jwt),
+    pubsub: RedisPubsub = Depends(get_pubsub),
 ):
     if not user_data:
         return
 
     try:
-        user_score = await scores_service.get_top_user_score(user_data.id)
-        # print(f"got user data: {user_score}")
-        result = user_score.model_dump() if user_score else None
-        await websocket.send_json({"result": result})
+
+        async def get_updated_user_score(data: Optional[str] = None):
+            await revalidate_token(websocket)
+            print(f"Got some data: {data}")
+            user_score = await scores_service.get_top_user_score(user_data.id)
+            result = user_score.model_dump() if user_score else None
+            await websocket.send_json({"result": result})
+
+        await get_updated_user_score()
+
+        await pubsub.subscribe(f"{USER_SCORE}{user_data.id}")
+
+        listener_task = pubsub.listen(get_updated_user_score)
+
+        while True:
+            # Just calling the receive_taxt to keep alive. Or use any other keep-alive mechanism
+            await websocket.receive_text()
+            # Optionally revalidate token periodically here if needed
+            # await asyncio.sleep(some_interval)
+
     except AsyncRedis.ConnectionError as e:
         logger.error(f"Redis connection error: {e}")
-    except Exception as e:
-        logger.error(f"WebSocket error: {e}")
-    finally:
-        logger.info("Websocket connection closed.")
+    except WebSocketDisconnect:
+        pubsub.unsubscribe(f"{USER_SCORE}{user_data.id}")
+        if listener_task and not listener_task.cancelled():
+            listener_task.cancel()
 
 
 @router.websocket("/add-score")
@@ -76,12 +95,13 @@ async def ws_submit_score(
                 ws_error = WsAppResponse(error="Invalid Request")
                 await websocket.send_json(ws_error.model_dump())
                 continue
-            
+
             # print(f"received data from client: {data}")
 
             await scores_service.add_user_score(data=data, user_data=user_data)
+            await redis.publish(SCORE_SUBMISSION, message=json.dumps(data.model_dump()))
             await redis.publish(
-                "score_submission", message=json.dumps(data.model_dump())
+                f"{USER_SCORE}{user_data.id}", message="UPDATE_SCORE_FETCH_FLAG"
             )
     except AsyncRedis.ConnectionError as e:
         logger.error(f"Redis connection error: {e}")
@@ -102,7 +122,7 @@ async def ws_get_scores(
         # gracefylly exit endpoint
         return
 
-    await pubsub.subscribe("score_submission")
+    await pubsub.subscribe(SCORE_SUBMISSION)
 
     try:
 
@@ -125,5 +145,5 @@ async def ws_get_scores(
         logger.error(f"Redis connection error: {e}")
     except WebSocketDisconnect:
         logger.info("Websocket Disconnected (/scores)")
-        await pubsub.unsubscribe("score_submission")
+        await pubsub.unsubscribe(SCORE_SUBMISSION)
         listener_task.cancel()
